bec5db754ffdd4659ac78283df52fbb2fbdb4e71
lrod <rleslie1015@gmail.com>
2019-05-18 19:08:56 -0700

    create react app

---
+++ lifegpa/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import Nav from "./Components/Nav";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Nav/> */}
+     <h1>Welcome to the new you.</h1>
+     <button>Create Account</button>
+     <button>Log In</button>
+    </div>
+  );
+}
+
+export default App;
