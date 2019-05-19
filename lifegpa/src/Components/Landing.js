6db8dfc4b6970b5a5c8aee0203631488ca4062a4
lrod <rleslie1015@gmail.com>
2019-05-18 21:30:29 -0700

    built register component

--- lifegpa/src/Components/Landing.js
+++ lifegpa/src/Components/Landing.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function Landing() {
     return (
     <div>
         <h1>Life GPA</h1>
         <h2>Welcome to the new you.</h2>
-        <button>Create Account</button>
+        <Link to="/register">
+            <button>Create Account</button>
+        </Link>
+        <Link to="/login">
         <button>Log In</button>
+        </Link>
     </div>
     )
 }
